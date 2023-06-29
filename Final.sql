77e3f423790597709ccf229a3e9f13eca45354b0
Enrique06078 <134745378+Enrique06078@users.noreply.github.com>
2023-06-28 23:31:48 -0600

    Add files via upload

---
+++ Final.sql
@@ -0,0 +1,95 @@
+create table tabla_1(
+id number(10),
+Nombre char(20),
+Precio number(10),
+Num_tipo number(10),
+Num_mar number(10));
+
+drop table tabla_1;
+
+create table tabla_2(
+id number(10),
+Tipo number(10));
+
+create table tabla_2(
+id number(10),
+Marca number(10));
+
+
+
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(02, 'Colchones', 20, 1, 1)
+insert into tabla_1 values(03, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(04, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(05, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(06, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(07, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(08, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(09, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(10, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(11, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(12, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(13, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(14, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(15, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(16, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(17, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(18, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(19, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
+insert into tabla_1 values(01, 'Donas Espolvoreadas', 20, 1, 1)
